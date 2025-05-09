f1206a63cc13e2f24f66e76e3e44d666df374e71
Chava Rodríguez <94005146+itschavaa@users.noreply.github.com>
2025-05-08 18:20:16 -0600

    fix: ajusta estilo de codificación para cumplir con PEP8

--- pacman.py
+++ pacman.py
@@ -1,5 +1,5 @@
 from random import choice
-from turtle import *
+import turtle
 
 from freegames import floor, vector
 
@@ -7,8 +7,8 @@ from freegames import floor, vector
 state = {'score': 0}
 
 # Turtle for drawing the map and score
-path = Turtle(visible=False)
-writer = Turtle(visible=False)
+path = turtle.Turtle(visible=False)
+writer = turtle.Turtle(visible=False)
 
 # Initial direction for pacman
 aim = vector(5, 0)
@@ -64,8 +64,8 @@ def square(x, y):
     path.end_fill()
 
 
-# Receives a point (vector) and returns a pair por numbers that translates the point position into a index of a 20 x 20
-# grid
+# Receives a point (vector) and returns a pair por numbers that translates the
+# point position into a index of a 20 x 20 grid
 def offset(point):
     """Return offset of point in tiles."""
     x = (floor(point.x, 20) + 200) / 20
@@ -93,7 +93,7 @@ def valid(point):
 # Draws the entire world (board)
 def world():
     """Draw world using path."""
-    bgcolor('black')
+    turtle.bgcolor('black')
     path.color('blue')
 
     for index in range(len(tiles)):
@@ -115,7 +115,7 @@ def move():
     """Move pacman and all ghosts."""
     writer.undo()
     writer.write(state['score'])  # Updates the score
-    clear()  # Clears the screen
+    turtle.clear()  # Clears the screen
 
     # Moves Pacman if the direction is valid
     if valid(pacman + aim):
@@ -130,31 +130,32 @@ def move():
         y = 180 - (index // 20) * 20
         square(x, y)
 
-    up()
-    goto(pacman.x + 10, pacman.y + 10)
-    dot(20, 'yellow')  # Draws Pacman
+    turtle.up()
+    turtle.goto(pacman.x + 10, pacman.y + 10)
+    turtle.dot(20, 'yellow')  # Draws Pacman
 
     # Moves the ghosts
     for point, course in ghosts:
         if valid(point + course):
             point.move(course)
         else:
-            options = [vector(5, 0), vector(-5, 0), vector(0, 5), vector(0, -5)]
+            options = [vector(5, 0), vector(-5, 0),
+                       vector(0, 5), vector(0, -5)]
             plan = choice(options)
             course.x = plan.x
             course.y = plan.y
-        up()
-        goto(point.x + 10, point.y + 10)
-        dot(20, 'red')  # Draws the ghost
+        turtle.up()
+        turtle.goto(point.x + 10, point.y + 10)
+        turtle.dot(20, 'red')  # Draws the ghost
 
-    update()  # Refreshes the screen
+    turtle.update()  # Refreshes the screen
 
     # Checks for collision between Pacman and ghosts
     for point, course in ghosts:
         if abs(pacman - point) < 20:
             return
 
-    ontimer(move, 50)  # Calls move() again after 50 ms
+    turtle.ontimer(move, 50)  # Calls move() again after 50 ms
 
 
 # Changes Pacman's direction if valid
@@ -165,21 +166,21 @@ def change(x, y):
 
 
 # Sets up the game window
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
 writer.goto(160, 160)
 writer.color('white')
 writer.write(state['score'])
 
 # Binds keys to Pacman's movement
-listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
+turtle.listen()
+turtle.onkey(lambda: change(5, 0), 'Right')
+turtle.onkey(lambda: change(-5, 0), 'Left')
+turtle.onkey(lambda: change(0, 5), 'Up')
+turtle.onkey(lambda: change(0, -5), 'Down')
 
 # Draws the map and starts the game
 world()
 move()
-done()
+turtle.done()
